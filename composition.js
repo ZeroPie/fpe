48426149742289e9ffb075a0b1a60a0fb2f81267
Pie <pie@Pies-iMac.fritz.box>
2018-12-02 12:26:55 +0100

    Initial Commit

---
+++ composition.js
@@ -0,0 +1,19 @@
+const suffix = suffixStr => input => input + ` ${suffixStr}`
+const pls = suffix('pls')
+const kthx = suffix('kthnks')
+const bye = suffix('bye ༼つಠ益ಠ༽つ ─=≡ΣO')
+const gitgud = suffix('gitgud')
+
+const memeify = x => bye(kthx(gitgud(pls(x))))
+console.log('memeify:', memeify('Teemo') );
+
+/* Using Ramda // REPL: https://ramdajs.com/repl/
+
+let composedMeme = R.compose(bye, kthx, gitgud, pls)
+console.log('composedMeme:', composedMeme('Rora'));
+
+// Alternative way if you want to read left to right
+let composedMeme = R.pipe(pls, gitgud, kthx, bye)
+
+console.log('composedMeme:', composedMeme('Rora'));
+*/
